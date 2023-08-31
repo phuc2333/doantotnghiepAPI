17ce01c5740034b8bff660b003c86665642e26c8
phuc.na1@extremevn.com.vn <phuc.na1@extremevn.com.vn>
2023-08-31 16:14:49 +0700

    datphongController

--- app/Http/Controllers/Admin/DatPhongController.php
+++ app/Http/Controllers/Admin/DatPhongController.php
@@ -8,26 +8,38 @@ use Illuminate\Http\Request;
 
 class DatPhongController extends Controller
 {
-    public function DatPhongTheoDoan(Request $request)
-    {
-        // xu ly lay du lieu danh sach dich vu dat
-        $dataDichVuDat_Phong_Khach_Hang = $request;
-        foreach ($dataDichVuDat_Phong_Khach_Hang["phong"] as $phong) {
-            foreach ($phong["dichVuDat"] as $dichVu) {
-                $datphong = new DatPhong();
-                $datphong->id_KH = $request->idKH;
-                $datphong->id_Phong = $phong['idPhong'];
-                $datphong->NgayDat = $request->NgayDat;
-                $datphong->NgayTra = $request->NgayTra;
-                $datphong->U_id = $request->U_id;
-                $datphong->status = $request->TrangThai;
-                $datphong->Ghichu = $request->Ghichu;
-                $datphong->TongTien = $phong['tongtien'];
+  public function DatPhongTheoDoan(Request $request)
+{
+    // Lấy dữ liệu đặt phòng từ request
+    $dataDichVuDat_Phong_Khach_Hang = $request->all();
+
+    // Lặp qua mảng các phòng và dịch vụ đặt
+    foreach ($dataDichVuDat_Phong_Khach_Hang['phong'] as $phongData) {
+        if (!empty($phongData['idPhong'])) {
+         
+            $datphong = new DatPhong();
+            $datphong->id_KH = $dataDichVuDat_Phong_Khach_Hang['idKH'];
+            $datphong->id_Phong = $phongData['idPhong'];
+            $datphong->NgayDat = $dataDichVuDat_Phong_Khach_Hang['NgayDat'];
+            $datphong->NgayTra = $dataDichVuDat_Phong_Khach_Hang['NgayTra'];
+            $datphong->U_id = $dataDichVuDat_Phong_Khach_Hang['U_id'];
+            $datphong->status = $dataDichVuDat_Phong_Khach_Hang['TrangThai'];
+            $datphong->Ghichu = $dataDichVuDat_Phong_Khach_Hang['Ghichu'];
+            $datphong->TongTien = $dataDichVuDat_Phong_Khach_Hang['tongtien'];
+            
+            //$datphong->save(); // Lưu thông tin đặt phòng
+            
+            // Lặp qua các dịch vụ đặt cho phòng này
+            foreach ($phongData["dichVuDat"] as $dichVu) {
+             
                 $datphong->SoLuongSanPham = $dichVu['soLuong'];
                 $datphong->id_SP = $dichVu['idsp'];
-                $datphong->save();
+                $datphong->save(); // Lưu thông tin dịch vụ đặt
             }
         }
-        return response()->json($request, 200);
     }
+    
+    return response()->json($request, 200);
+}
+
 }
